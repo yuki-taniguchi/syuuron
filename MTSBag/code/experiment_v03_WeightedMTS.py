29f1621acf4e5c4ab27797e80fb2cfe1faa39a76
yukitaniguchi <baseball.t.yuki@gmail.com>
2022-04-20 18:13:36 +0900

    0420実験完了

--- MTSBag/code/experiment_v03_WeightedMTS.py
+++ MTSBag/code/experiment_v03_WeightedMTS.py
@@ -23,142 +23,142 @@ ex_name = input('実験名は?')
 n_experiment = 10
 data_list = [
     'yeast', 
-    'wine', 
-    'abalone', 
-    'car',
-    'cancer', 
-    'letter'
+    # 'wine', 
+    # 'abalone', 
+    # 'car',
+    # 'cancer', 
+    # 'letter'
     ]
 
 
 
 
-print('MTS開始！')
+# print('MTS開始！')
 
-for data in data_list:
-    print(data)
-    result_df = pd.DataFrame(
-                        columns=['AUC', 'accuracy', 'recall', 'Specificity', 'precision', 'gmeans', 'RS'],
-                        index=range(n_experiment))
-    for m in tqdm(range(n_experiment)):
+# for data in data_list:
+#     print(data)
+#     result_df = pd.DataFrame(
+#                         columns=['AUC', 'accuracy', 'recall', 'Specificity', 'precision', 'gmeans', 'RS'],
+#                         index=range(n_experiment))
+#     for m in tqdm(range(n_experiment)):
         
-        X, y = data_load(data)
+#         X, y = data_load(data)
 
-        # バギング側の話
-        # ブートストラップサンプリングの個数
-        n = 10
-        seed = random.randint(0, n)
+#         # バギング側の話
+#         # ブートストラップサンプリングの個数
+#         n = 10
+#         seed = random.randint(0, n)
 
-        # 使用する7つの変数をランダムに取得する
-        # バギングをする際はそれぞれのサブサンプルで7つの変数を選択する．
-        random.seed(seed)
-        random_s = random.sample(list(X.columns), len(X.columns) if len(X.columns) < 7 else 7)
-        X = X[random_s]
+#         # 使用する7つの変数をランダムに取得する
+#         # バギングをする際はそれぞれのサブサンプルで7つの変数を選択する．
+#         random.seed(seed)
+#         random_s = random.sample(list(X.columns), len(X.columns) if len(X.columns) < 7 else 7)
+#         X = X[random_s]
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        result_scaler, result_inv_C, select_columns = fit_MTS(X_train, y_train)
+#         result_scaler, result_inv_C, select_columns = fit_MTS(X_train, y_train)
 
-        y_train_pred = predict_MD(X_train, result_scaler, result_inv_C, select_columns)
+#         y_train_pred = predict_MD(X_train, result_scaler, result_inv_C, select_columns)
 
-        threshold = determine_threshold(y_train, y_train_pred)
+#         threshold = determine_threshold(y_train, y_train_pred)
 
-        y_proba = predict_MD(X_test, result_scaler, result_inv_C, select_columns)
-        y_pred = y_proba > threshold
+#         y_proba = predict_MD(X_test, result_scaler, result_inv_C, select_columns)
+#         y_pred = y_proba > threshold
 
-        result_df = make_result_df(result_df, y_test, y_pred, y_proba, m)
+#         result_df = make_result_df(result_df, y_test, y_pred, y_proba, m)
         
-    result_df.to_csv(f'../data/output/{ex_name}_MTS_{data}_result.csv')
+#     result_df.to_csv(f'../data/output/{ex_name}_MTS_{data}_result.csv')
 
 
-print('WMTS開始！')
+# print('WMTS開始！')
 
-for data in data_list:
-    print(data)
-    result_df = pd.DataFrame(
-                        columns=['AUC', 'accuracy', 'recall', 'Specificity', 'precision', 'gmeans', 'RS'],
-                        index=range(n_experiment))
-    for m in tqdm(range(n_experiment)):
+# for data in data_list:
+#     print(data)
+#     result_df = pd.DataFrame(
+#                         columns=['AUC', 'accuracy', 'recall', 'Specificity', 'precision', 'gmeans', 'RS'],
+#                         index=range(n_experiment))
+#     for m in tqdm(range(n_experiment)):
         
-        X, y = data_load(data)
+#         X, y = data_load(data)
 
-        # バギング側の話
-        # ブートストラップサンプリングの個数
-        n = 10
-        seed = random.randint(0, n)
+#         # バギング側の話
+#         # ブートストラップサンプリングの個数
+#         n = 10
+#         seed = random.randint(0, n)
 
-        # 使用する7つの変数をランダムに取得する
-        # バギングをする際はそれぞれのサブサンプルで7つの変数を選択する．
-        random.seed(seed)
-        random_s = random.sample(list(X.columns), len(X.columns) if len(X.columns) < 7 else 7)
-        X = X[random_s]
+#         # 使用する7つの変数をランダムに取得する
+#         # バギングをする際はそれぞれのサブサンプルで7つの変数を選択する．
+#         random.seed(seed)
+#         random_s = random.sample(list(X.columns), len(X.columns) if len(X.columns) < 7 else 7)
+#         X = X[random_s]
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        reduced_model_scaler, reduced_model_inv_C, select_columns, select_columns_weight = fit_WMTS(X_train, y_train)
+#         reduced_model_scaler, reduced_model_inv_C, select_columns, select_columns_weight = fit_WMTS(X_train, y_train)
 
-        y_train_pred = cal_WMD_by_reduced_model(X_train, reduced_model_scaler, reduced_model_inv_C, select_columns, select_columns_weight)
+#         y_train_pred = cal_WMD_by_reduced_model(X_train, reduced_model_scaler, reduced_model_inv_C, select_columns, select_columns_weight)
 
-        threshold = determine_threshold(y_train, y_train_pred)
+#         threshold = determine_threshold(y_train, y_train_pred)
 
-        proba, pred = predict_WMTS(X_test, reduced_model_scaler, reduced_model_inv_C, select_columns, select_columns_weight, threshold)
+#         proba, pred = predict_WMTS(X_test, reduced_model_scaler, reduced_model_inv_C, select_columns, select_columns_weight, threshold)
 
-        result_df = make_result_df(result_df, y_test, pred, proba, m)
+#         result_df = make_result_df(result_df, y_test, pred, proba, m)
         
-    result_df.to_csv(f'../data/output/{ex_name}_WMTS_{data}_result.csv')
+#     result_df.to_csv(f'../data/output/{ex_name}_WMTS_{data}_result.csv')
 
 
 
-print('ImpAggMTSBag開始')
+# print('ImpAggMTSBag開始')
 
-for data in data_list:
-    print(data)
-    result_df = pd.DataFrame(
-                        columns=['AUC', 'accuracy', 'recall', 'Specificity', 'precision', 'gmeans', 'RS'],
-                        index=range(n_experiment))
-    for m in tqdm(range(n_experiment)):
+# for data in data_list:
+#     print(data)
+#     result_df = pd.DataFrame(
+#                         columns=['AUC', 'accuracy', 'recall', 'Specificity', 'precision', 'gmeans', 'RS'],
+#                         index=range(n_experiment))
+#     for m in tqdm(range(n_experiment)):
         
-        X, y = data_load(data)
+#         X, y = data_load(data)
 
-        # パラメータ
-        n_estimators = 10
-        max_samples = 0.5
+#         # パラメータ
+#         n_estimators = 10
+#         max_samples = 0.5
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        # 実行するところ
+#         # 実行するところ
 
-        # n_estimators:再標本化の回数 SIZE:再標本化されたもののサンプルサイズ
-        SIZE = int(len(X) * max_samples)
+#         # n_estimators:再標本化の回数 SIZE:再標本化されたもののサンプルサイズ
+#         SIZE = int(len(X) * max_samples)
 
-        # 予測に必要なパラメータ
-        select_columns = [0] * n_estimators
-        result_scaler = [0] * n_estimators
-        result_inv_C = [0] * n_estimators
-        threshold = [0] * n_estimators
+#         # 予測に必要なパラメータ
+#         select_columns = [0] * n_estimators
+#         result_scaler = [0] * n_estimators
+#         result_inv_C = [0] * n_estimators
+#         threshold = [0] * n_estimators
 
-        for i in range(n_estimators):
-            # bootstrap sampling
-            resampled_data_x, resampled_data_y = resample(X_train, y_train, n_samples = SIZE)
-            random_s = random.sample(
-                list(resampled_data_x.columns), 
-                len(resampled_data_x.columns) 
-                if len(resampled_data_x.columns) < 7 
-                else 7
-                )
-            resampled_data_x = resampled_data_x[random_s]
+#         for i in range(n_estimators):
+#             # bootstrap sampling
+#             resampled_data_x, resampled_data_y = resample(X_train, y_train, n_samples = SIZE)
+#             random_s = random.sample(
+#                 list(resampled_data_x.columns), 
+#                 len(resampled_data_x.columns) 
+#                 if len(resampled_data_x.columns) < 7 
+#                 else 7
+#                 )
+#             resampled_data_x = resampled_data_x[random_s]
 
-            result_scaler[i], result_inv_C[i], select_columns[i] = fit_MTS(resampled_data_x, resampled_data_y)
+#             result_scaler[i], result_inv_C[i], select_columns[i] = fit_MTS(resampled_data_x, resampled_data_y)
 
-            y_train_pred = predict_MD(resampled_data_x, result_scaler[i], result_inv_C[i], select_columns[i])
+#             y_train_pred = predict_MD(resampled_data_x, result_scaler[i], result_inv_C[i], select_columns[i])
 
-            threshold[i] = determine_threshold(resampled_data_y, y_train_pred)
+#             threshold[i] = determine_threshold(resampled_data_y, y_train_pred)
             
-        y_proba, y_pred = predict_MTSBag_ImpAgg(X_test, result_scaler, result_inv_C, select_columns, threshold, n_estimators)
+#         y_proba, y_pred = predict_MTSBag_ImpAgg(X_test, result_scaler, result_inv_C, select_columns, threshold, n_estimators)
 
-        result_df = make_result_df(result_df, y_test, y_pred, y_proba, m)
+#         result_df = make_result_df(result_df, y_test, y_pred, y_proba, m)
         
-    result_df.to_csv(f'../data/output/{ex_name}_MTSBagImpAgg_{data}_result.csv')
+#     result_df.to_csv(f'../data/output/{ex_name}_MTSBagImpAgg_{data}_result.csv')
 
 
 print('ImpAggWMTSBag開始')
